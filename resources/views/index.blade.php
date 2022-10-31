ef69b7472c5eb1c23dee8b6299d56379db58a14c
noemievincent <noemie.vincent1@student.hepl.be>
2022-10-31 09:25:54 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Design</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<h1>
+    Web Design - formation web
+</h1>
+<header>
+    <div>
+
+    </div>
+    <div>
+        <nav>
+
+        </nav>
+    </div>
+</header>
+</body>
+</html>
