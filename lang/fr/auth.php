e0b7b5aa963762fdac1c2fca80951c6842906314
noemievincent <noemie.vincent1@student.hepl.be>
2022-12-21 13:18:31 +0100

    translated fr default files

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -13,9 +13,9 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ces informations d’identification ne correspondent pas à nos dossiers.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds seconds.',
 
 
     'login' => [
